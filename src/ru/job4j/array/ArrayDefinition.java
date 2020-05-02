ca270b6a7f61699e9fd20aa3af916caa3d90f9d0
Alexey Cheremisin <71m25@mail.ru>
2020-05-02 23:43:01 +0300

    6.0.1. Объявление массива.[#281712]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+/**
+ * ArrayDefinition класс для отработки создания массивов
+ * @author Alexey Cheremisin
+ * @since 02.05.2020
+ * @version 1
+ */
+public class ArrayDefinition {
+    /**
+     * main - точка входа в программу. Содержит примеры создания массивов.
+     * @param args - массив строк
+     */
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
