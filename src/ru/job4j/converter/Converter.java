4d323a8248a4f4ecd54c137ebd64ba4d3832f013
Alexey Cheremisin <71m25@mail.ru>
2020-05-02 10:38:33 +0300

    5.8. JavaDoc. Code convention[#281776]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,16 +1,35 @@
 package ru.job4j.converter;
 
+/**
+ * Converter Класс для конвертации раблей в евро и доллар.
+ *
+ * @author Alexey Cheremisin
+ * @since 30.04.2020
+ * @version 1
+ */
 public class Converter {
+    /**
+     * rubleToEuro переводит рубли в евро.
+     * @param value - количество рублей.
+     * @return количество евро по курсу euro = 70 rub.
+     */
     public static int rubleToEuro(int value) {
-        int rsl = value / 70;
-        return rsl;
+        return value / 70;
     }
 
+    /**
+     * rubleToDollar переводит рубли доллары.
+     * @param value - количество рублей.
+     * @return количество долларов по курсу dollar = 60 rub.
+     */
     public static int rubleToDollar(int value) {
-        int rsl = value / 60; // формула перевода рублей в доллоры.
-        return rsl;
+        return value / 60;
     }
 
+    /**
+     * main точка входа в программу с парой тестов внутри
+     * @param args - массив строк
+     */
     public static void main(String[] args) {
         int in = 140;
         int expected = 2;
