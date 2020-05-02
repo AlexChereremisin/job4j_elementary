8dc3112a3992eeb9a382020a3f9d4174e3266c95
Alexey Cheremisin <71m25@mail.ru>
2020-05-02 10:53:09 +0300

    5.9. Крест в псевдографике. [#281769]

--- src/ru/job4j/loop/Slash.java
+++ src/ru/job4j/loop/Slash.java
@@ -1,11 +1,21 @@
 package ru.job4j.loop;
 
+/**
+ * Slash Класс для рисования креста Х в псевдографике.
+ * @author Alexey Cheremisin
+ * @since 02.05.2020
+ * @version 1
+ */
 public class Slash {
+    /**
+     * draw метод для отрисовки креста Х в псевдографике.
+     * @param size - размер стороны квадратного поля в котором будет рисоваться крест.
+     */
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = row == cell; // добавить условие, по которому нужно определить ставить ли символ или нет.
-                boolean right = cell == ((size - 1) - row); // добавить условие, что нужно ставить элемент в правый угол.
+                boolean left = row == cell;
+                boolean right = cell == ((size - 1) - row);
                 if (left) {
                     System.out.print("0");
                 } else if (right) {
@@ -19,6 +29,10 @@ public class Slash {
         System.out.println();
     }
 
+    /**
+     * main точка входа в программу с тестами метода draw внутри.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         System.out.println("Draw by 3");
         draw(3);
