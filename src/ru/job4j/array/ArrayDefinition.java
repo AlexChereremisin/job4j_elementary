49d11f28ba65443292071de97ae3e3710fc90f7f
Alexey Cheremisin <71m25@mail.ru>
2020-05-02 23:49:25 +0300

    6.0.2. Заполнение массива.[#281704]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -15,5 +15,11 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Andrey Petrov";
+        names[2] = "Sergey Tolstov";
+        names[3] = "Svetlana Kuznetsova";
     }
 }
