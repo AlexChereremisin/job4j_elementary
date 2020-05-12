b811d56827c4e38eeef050a81fe72786e819f7db
Alexey Cheremisin <71m25@mail.ru>
2020-05-12 16:41:08 +0300

    3. Удаление моделей из массива.(упростил тело метода Shop.delete) [#281878]

--- src/ru/job4j/pojo/Shop.java
+++ src/ru/job4j/pojo/Shop.java
@@ -29,13 +29,10 @@ public class Shop {
 
     public static Product[] delete(Product[] products, int index) {
         products[index] = null;
-        for (int i = index; i < products.length; i++) {
-            if (i != products.length - 1) {
-                products[i] = products[i + 1];
-            } else {
-                products[i] = null;
-            }
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
         }
+        products[products.length - 1] = null;
         return products;
     }
 }
