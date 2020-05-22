5c6338013c594865608e432c42c0117b2507308f
Alexey Cheremisin <71m25@mail.ru>
2020-05-22 12:08:41 +0300

    1. Телефонный справочник на базе ArrayList [#281793]

---
+++ test/ru/job4j/collection/search/PhoneDictionaryTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.collection.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByOneChar() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        phones.add(
+                new Person("Alexey", "Cheremisin", "4567899", "Nizhny Novgorod")
+        );
+        ArrayList<Person> persons = phones.find("4");
+        assertThat(persons.size(), is(2));
+    }
+}
