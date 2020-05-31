b8cff8f6fcb5e57417383f49674a850f70c1153b
Alexey Cheremisin <71m25@mail.ru>
2020-05-31 23:27:56 +0300

    4. Преобразование List в Map. [#281660]

---
+++ src/ru/job4j/lambda/MappingStudents.java
@@ -0,0 +1,17 @@
+package ru.job4j.lambda;
+
+import ru.job4j.lambda.filter.Student;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MappingStudents {
+    public static Map<String, Student> studentsToMap(final List<Student> studentList) {
+        return studentList.stream()
+                .collect(Collectors.toMap(
+                        Student::getSurname,
+                        s -> s
+                ));
+    }
+}
---
+++ test/ru/job4j/lambda/MappingStudentsTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+import ru.job4j.lambda.filter.Student;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class MappingStudentsTest {
+    @Test
+    public void studentsToMap() {
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(new Student("Petrova", 98));
+        studentList.add(new Student("Udalova", 70));
+        Map<String, Student> expected = new HashMap<>();
+        expected.put(studentList.get(0).getSurname(), studentList.get(0));
+        expected.put(studentList.get(1).getSurname(), studentList.get(1));
+        Map<String, Student> result = new HashMap<>(MappingStudents.studentsToMap(studentList));
+        assertThat(result, is(expected));
+    }
+}
